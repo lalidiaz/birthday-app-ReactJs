d5f4c3fead1249480b22b595e76770c6cd276761
Lali <lauradiaz1586@gmail.com>
2021-02-15 13:24:33 +0400

    add List component, add database and set up project.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>soy app</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'Justin Perkins',
+    age: 29,
+    image: 'https://randomuser.me/api/portraits/men/17.jpg'
+  },
+  {
+    id: 2,
+    name: 'Bella Jones',
+    age: 32,
+    image: 'https://randomuser.me/api/portraits/women/59.jpg'
+  },
+  {
+    id: 3,
+    name: 'Erika Grant',
+    age: 36,
+    image: 'https://randomuser.me/api/portraits/women/21.jpg'
+  },
+  {
+    id: 4,
+    name: 'Neil Mckinney',
+    age: 34,
+    image: 'https://randomuser.me/api/portraits/men/29.jpg'
+  },
+  {
+    id: 5,
+    name: 'Sandra Price',
+    age: 29,
+    image: 'https://randomuser.me/api/portraits/women/57.jpg'
+  }
+];
